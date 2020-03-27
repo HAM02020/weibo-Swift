0a81da439a5bd482e0b1cd2e7c264e8b7b166e99
HAM02020 <1106201802@qq.com>
2020-03-27 16:39:04 +0800

    搭建数据框架

--- weibo/Classes/VIew/Main/WBBaseViewController.swift
+++ weibo/Classes/VIew/Main/WBBaseViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
-
+//继承协议
 class WBBaseViewController: UIViewController {
+    
+    
     /// 表格视图  如果用户没有登陆就不显示
     var tableView : UITableView?
     
@@ -33,11 +35,37 @@ extension WBBaseViewController {
     
     @objc func setupUI() {
         view.backgroundColor = UIColor.white
-
+        loadData()
     }
     //设置d表格视图
     func setupTalbeView() {
         tableView = UITableView(frame: view.bounds, style: .plain)
         view.addSubview(tableView!)
+        
+        //设置数据源 和 代理 -> 目的 子类直接实现h数据源方法
+        //tableView?.dataSource = self
+        //tableView?.delegate = self
     }
+    ///加载数据 - 由子类去实现
+    func loadData() {
+        
+    }
+}
+//MARK: -实现基类数据源方法 子类实现不用super
+extension WBBaseViewController:UITableViewDataSource,UITableViewDragDelegate {
+    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        return []
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    
+    
 }
