65b065201b5a9190c315d62dbef7463feca84702
mo <mo@mo.com>
2020-03-31 21:01:03 +0800

    微博cell连线

--- weibo/Classes/VIew/Home/StatusCell/WBStatusCell.swift
+++ weibo/Classes/VIew/Home/StatusCell/WBStatusCell.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class WBStatusCell: UITableViewCell {
 
-    
-    
+    ///头像
+    @IBOutlet weak var iconView: UIImageView!
+    ///姓名
+    @IBOutlet weak var nameLabel: UILabel!
+    ///皇冠图标
+    @IBOutlet weak var memberIconView: UIImageView!
+    ///时间
+    @IBOutlet weak var timeLabel: UILabel!
+    ///来源
+    @IBOutlet weak var sourceLabel: UILabel!
+    ///认证图标
+    @IBOutlet weak var vipIcon: UIImageView!
+    ///微博正文
+    @IBOutlet weak var statusLabel: UILabel!
     
     
     override func awakeFromNib() {
